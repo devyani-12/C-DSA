aa56fa30699b47b50c604004b1f63f980d910115
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-03-05 15:10:04 +0530

    "A---65 B---66"

---
+++ convertchtoascii.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    char ch='A';
+    
+    for(int i=0;i<7;i++){
+        cin>>ch;
+        cout<<(int)ch;
+    }
+    
+
+    return 0;
+}
